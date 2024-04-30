08e9a55d567e4b7cd97c74f2243a8077f440fd9d
samshar123 <samsharsachu1@gmail.com>
2024-04-30 11:23:04 +0530

    page corrected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Rome from "./Home/page";
 
 export default function Home() {
   return (
-   <div>
-
-
-   </div>
+    <div>
+      <Rome />
+    </div>
   );
 }
