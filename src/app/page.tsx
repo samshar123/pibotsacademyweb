68226aaffb0793ab60735f3b8dd8c34a4052f6a8
samshar123 <samsharsachu1@gmail.com>
2024-04-30 13:04:58 +0530

    changed2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Rome from "./Rome/page";
+import Rome from "./Home/page";
 
 export default function Home() {
   return (
